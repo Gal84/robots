0f74dc1e50ea73652a7f230ccc75bf70b4f3e859
Gal Gorelik <jenya_g@msn.com>
2020-07-03 23:34:27 +0300

    react robot

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Scroll = (props) => {
   return (
-    <div style={{ overflow: 'scroll', border: '5px solid black', height: '800px'}}>
+    <div style={{ overflow: 'scroll', border: '3px solid black', maxWidth: "1450px", margin: '0 auto', height: '550px' }}>
       {props.children}
     </div>
   );
