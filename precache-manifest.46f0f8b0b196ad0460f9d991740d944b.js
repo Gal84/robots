7cd401abb6947ab16d85bc06a4e143aad5eda4a1
Gal Gorelik <jenya_g@msn.com>
2020-07-04 02:19:01 +0300

    Updates

---
+++ precache-manifest.46f0f8b0b196ad0460f9d991740d944b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65acc6a038a7e6b92c16917d5ed37e3b",
+    "url": "/robots/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/robots/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "2b5dc1db4eb87d06b273",
+    "url": "/robots/static/css/main.3e6dbbdc.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/robots/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robots/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b5dc1db4eb87d06b273",
+    "url": "/robots/static/js/main.aecef6c4.chunk.js"
+  },
+  {
+    "revision": "df9e66469025a98d5aad",
+    "url": "/robots/static/js/runtime-main.e1a61dd3.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robots/static/media/SEGA.12a3db8b.woff"
+  }
+]);
