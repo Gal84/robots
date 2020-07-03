3ca1a5da53d703b7079e4e035a9694c558c60a25
Gal Gorelik <jenya_g@msn.com>
2020-07-04 00:13:49 +0300

    added overflowY

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 const Scroll = (props) => {
   return (
-    <div style={{ overflow: 'scroll', border: '3px solid black', maxWidth: "1450px", margin: '0 auto', height: '550px' }}>
+    <div style={{
+      overflowY: 'scroll', border: '3px solid black',
+      maxWidth: "1450px", margin: '0 auto', height: '550px'
+    }}>
       {props.children}
     </div>
   );
