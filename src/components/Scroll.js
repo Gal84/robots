fae35ce7e72b30c1ea99bbeef0e0a6354c97eb55
Gal Gorelik <jenya_g@msn.com>
2020-07-04 13:48:29 +0300

    some updates

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -4,7 +4,7 @@ const Scroll = (props) => {
   return (
     <div style={{
       overflowY: 'scroll', border: '3px solid black',
-      maxWidth: "1450px", margin: '0 auto', height: '555px'
+      maxWidth: "1450px", margin: '0 auto', height: '500px'
     }}>
       {props.children}
     </div>
